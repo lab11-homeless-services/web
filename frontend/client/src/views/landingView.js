64871f985f3d76fee67913c28ae242f4080d96ca
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-29 18:36:48 -0400

    Another test div

--- frontend/client/src/views/landingView.js
+++ frontend/client/src/views/landingView.js
@@ -3,21 +3,23 @@ import SearchBar from "../components/searchBar";
 import { Link } from "react-router-dom";
 
 class LandingView extends React.Component {
-
-   
-    render() {
-        console.log(this.props.spanish)
-        return(
-            <div>
-                <SearchBar />
-                <div className="language">
-                    <Link to='/home'><div>English</div></Link>
-                    <Link to='/home'><div onClick = {this.props.setSpanish}>Español?</div></Link>
-                </div>
-            </div>
-        )
-    }
-
+  render() {
+    console.log(this.props.spanish);
+    return (
+      <div>
+        <SearchBar />
+        <div className="language">
+          <Link to="/home">
+            <div>English</div>
+          </Link>
+          <Link to="/home">
+            <div onClick={this.props.setSpanish}>Español?</div>
+          </Link>
+          <div>Is this Test Div working and Testing Travis? Let's hope...</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LandingView;
