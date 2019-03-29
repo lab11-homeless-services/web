08f27ba1ab1202405fa108df84486d577db60719
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-29 18:04:23 -0400

    Added test div to landingView to test Travis CI

--- frontend/client/src/views/landingView.js
+++ frontend/client/src/views/landingView.js
@@ -1,32 +1,37 @@
-import React from 'react'
-import SearchBar from '../components/searchBar'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from "react";
+import SearchBar from "../components/searchBar";
+import { Link } from "react-router-dom";
 
 class LandingView extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            spanish: false
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      spanish: false
+    };
+  }
 
-    setSpanish = e => {
-        this.setState({
-            spanish: true
-        })
-    }
+  setSpanish = e => {
+    this.setState({
+      spanish: true
+    });
+  };
 
-    render() {
-        return(
-            <div>
-                <SearchBar />
-                <div className="language">
-                    <Link to='/home'><div>English</div></Link>
-                    <Link to='/home'><div onClick = {this.setSpanish}>Español?</div></Link>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <div className="language">
+          <Link to="/home">
+            <div>English</div>
+          </Link>
+          <Link to="/home">
+            <div onClick={this.setSpanish}>Español?</div>
+          </Link>
+        </div>
+        <div>Hey is this div rendering? -Josh</div>
+      </div>
+    );
+  }
 }
 
-export default LandingView
+export default LandingView;
