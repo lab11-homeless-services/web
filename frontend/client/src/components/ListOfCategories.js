94c0fbaf8238774c47a0a8a9d270d7c754f84ee9
matt0418 <mfeldma7@gmail.com>
2019-04-02 12:09:39 -0400

    added ability to get spanish category lists

--- frontend/client/src/components/ListOfCategories.js
+++ frontend/client/src/components/ListOfCategories.js
@@ -1,24 +1,48 @@
 import React from 'react'
 import useFetch from '../functions/useFetch'
+import { useStateValue } from '../state/state'
 
 const ListOfCats = () => {
-    const categories = useFetch('https://empact-e511a.firebaseio.com/categories.json')
-    const test = categories.category_name
-    
-    if (test !== undefined) {
-        return(
-            <div>
-                {categories.category_name.map(category => (
-                    <div>
-                        {category}
-                    </div>
-                ))}
-            </div>
+    const [{language}] = useStateValue()
+    const categoriesEng = useFetch('https://empact-e511a.firebaseio.com/categories.json')
+    const categoriesSpa = useFetch('https://empact-e511a.firebaseio.com/categories_espanol.json')
+    console.log(language.spanish)
+
+    if(language.spanish === true) {
+        const test = categoriesSpa.category_name
+        if (test !== undefined) {
+            return(
+                <div>
+                    {categoriesSpa.category_name.map(category => (
+                        <div>
+                            {category}
+                        </div>
+                    ))}
+                </div>
+            )
+        }
+        return (
+            <div>Loading</div>
         )
+    } else {
+        const test = categoriesEng.category_name
+        if(test !== undefined) {
+            return(
+                <div>
+                    {categoriesEng.category_name.map(category => (
+                        <div>
+                            {category}
+                        </div>
+                    ))}
+                </div>
+            )
+        } 
+        return (
+            <div>Loading</div>
+        )
+        
+
     }
-    return (
-        <div>Loading</div>
-    )
     
 }
 
