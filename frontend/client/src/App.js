e9bb5214cffb9cbd5abeb02bfa7d2e603b9d78db
matt0418 <mfeldma7@gmail.com>
2019-04-02 11:21:32 -0400

    refactored app with state globally

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,54 +1,50 @@
-import React, { Component } from "react";
+import React, { useState, Component } from "react";
 import { Route, NavLink } from "react-router-dom";
 import "./App.css";
 import LandingView from "./views/landingView";
 import CategoriesView from "./views/categoriesView";
+import {StateProvider} from './state/state'
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      spanish: false
-    };
-  }
-
-  setSpanish = () => {
-    if (this.state.spanish === false) {
-      this.setState({
-        spanish: true
-      });
-    } else {
-      this.setState({
-        spanish: false
-      });
-    }
-  };
-
   render() {
+    const initialState = {
+      language: {
+        spanish: false
+      }
+    }
+
+    const reducer = (state, action) => {
+      switch (action.type) {
+        case 'setSpanish':
+        if (state.language.spanish === false) {
+          return {
+            ...state,
+            language: {spanish: true}
+          }
+        }
+          return {
+            ...state,
+            language: {spanish: false}
+          }
+          default: 
+            return state
+        }
+      }
+    
     return (
-      <div className="App">
-        <Route
-          exact
-          path="/"
-          render={props => (
-            <LandingView
-              {...props}
-              setSpanish={this.setSpanish}
-              spanish={this.state.spanish}
-            />
-          )}
-        />
-        <Route
-          path="/home"
-          render={props => (
-            <CategoriesView
-              {...props}
-              setSpanish={this.setSpanish}
-              spanish={this.state.spanish}
-            />
-          )}
-        />
-      </div>
+      <StateProvider initialState={initialState} reducer={reducer}>
+        <div className="App">
+          <Route
+            exact
+            path="/"
+            render={props => (<LandingView {...props}/>)}
+          />
+          <Route
+            path="/home"
+            render={props => (<CategoriesView {...props}/>)}
+          />
+        </div>
+      </StateProvider>
     );
   }
 }
--- frontend/client/src/views/landingView.js
+++ frontend/client/src/views/landingView.js
@@ -1,24 +1,48 @@
 import React, { Fragment } from "react";
 import SearchBar from "../components/searchBar";
 import { Link } from "react-router-dom";
+import { useStateValue } from '../state/state'
 
-class LandingView extends React.Component {
-  render() {
-    console.log(this.props.spanish);
-    return (
-      <div>
-        <SearchBar />
-        <div className="language">
-          <Link to="/home">
-            <div>English</div>
-          </Link>
-          <Link to="/home">
-            <div onClick={this.props.setSpanish}>Español?</div>
-          </Link>
-        </div>
-      </div>
-    );
-  }
+// class LandingView extends React.Component {
+//   render() {
+//     console.log(this.props.spanish);
+//     return (
+//       <div>
+//         <SearchBar />
+//         <div className="language">
+//           <Link to="/home">
+//             <div>English</div>
+//           </Link>
+//           <Link to="/home">
+//             <div onClick={this.props.setSpanish}>Español?</div>
+//           </Link>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+const LandingView = () => {
+  const [{language}, dispatch] = useStateValue()
+  console.log(language)
+  return (
+    <div>
+         <SearchBar />
+         <div className="language">
+           <Link to="/home">
+             <div>English</div>
+           </Link>
+           <Link to="/home">
+             <div spanish={language.spanish}
+
+             onClick={() => dispatch({
+                type: 'setSpanish',
+                language: true
+              })}>Español?</div>
+           </Link>
+         </div>
+       </div>
+  )
 }
 
 export default LandingView;
