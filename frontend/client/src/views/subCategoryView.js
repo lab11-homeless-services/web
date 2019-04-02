fc8e391124a1bf9575f7ca26eb0277861af4daa6
matt0418 <mfeldma7@gmail.com>
2019-04-02 11:54:47 -0400

    begun building subcategory view

---
+++ frontend/client/src/views/subCategoryView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const subCategoryView = () => {
+    return(
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default subCategoryView
