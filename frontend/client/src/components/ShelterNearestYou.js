f1822796296a1f69eecdebffab8ff3c9ff804dcd
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-04-03 17:50:04 -0400

    Created Shelters Near you component

---
+++ frontend/client/src/components/ShelterNearestYou.js
@@ -0,0 +1,14 @@
+import React, { useState, Component } from "react";
+import { Route, NavLink } from "react-router-dom";
+import MapContainer from "./MapContainer";
+
+const ShelterNearestYou = props => {
+  console.log("shelter near you", props);
+  return null;
+  // <MapContainer
+  //   lat={props.currentlocation.lat}
+  //   lng={props.currentlocation.lng}
+  // />
+};
+
+export default ShelterNearestYou;
