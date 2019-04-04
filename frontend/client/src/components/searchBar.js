bf9fa2affac53277a97c1d5d9aa43fc37b38915e
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-04-04 07:57:50 -0400

    Commenting out algoliasearch for troubleshooting

--- frontend/client/src/components/searchBar.js
+++ frontend/client/src/components/searchBar.js
@@ -1,17 +1,20 @@
 import React from "react";
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch,
-  Hits,
-  SearchBox,
-  Pagination,
-  Highlight,
-  ClearRefinements,
-  RefinementList,
-  Configure, } from 'react-instantsearch-dom';
-  const searchClient = algoliasearch(
-    '1CH1AN74EQ',
-    '357f19a4594471c0f7d04a1a2bde1e8d'
-  );
+// import algoliasearch from "algoliasearch/lite";
+// import {
+//   InstantSearch,
+//   Hits,
+//   SearchBox,
+//   Pagination,
+//   Highlight,
+//   ClearRefinements,
+//   RefinementList,
+//   Configure
+// } from "react-instantsearch-dom";
+
+// const searchClient = algoliasearch(
+//   '1CH1AN74EQ',
+//   '357f19a4594471c0f7d04a1a2bde1e8d'
+// );
 
 class SearchBar extends React.Component {
   render() {
