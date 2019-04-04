837972080cecff6f3afbab56120ebc7198ec2c3a
matt0418 <mfeldma7@gmail.com>
2019-04-03 23:01:05 -0400

    changes to some of the state management regarding reducers

--- frontend/client/src/functions/useFetchSingle.js
+++ frontend/client/src/functions/useFetchSingle.js
@@ -20,39 +20,3 @@ export default function useFetchSingle(url) {
     },[url])
     return data
 }
-
-// export default function useEndpoint(req) {
-//     const [res, setRes] = useState({
-//       data: null,
-//       pending: false,
-//       completed: false,
-//       error: false,
-//     });
-    
-//     useEffect(() => {
-//       setRes({
-//         data: null,
-//         pending: true,
-//         completed: false,
-//         error: false,
-//       });
-//       axios(req)
-//         .then(res =>
-//           setRes({
-//             data: res.data,
-//             pending: false,
-//             error: false,
-//             complete: true
-//           }),
-//         )
-//         .catch(() =>
-//           setRes({
-//             data: null,
-//             pending: false,
-//             error: true,
-//             complete: true
-//           }),
-//         );
-//     }, [req.url]);
-//     return res;
-//   }
