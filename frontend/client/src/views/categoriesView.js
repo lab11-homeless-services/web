4b8a7d8424a559cbac7a313c2aba32c3d128f6f7
matt0418 <mfeldma7@gmail.com>
2019-04-09 11:58:45 -0400

    refactored categories to use hooks and simplified maps

--- frontend/client/src/views/categoriesView.js
+++ frontend/client/src/views/categoriesView.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useReducer, useEffect, useContext} from "react";
 import ReactDOM from "react-dom";
 import Header from "../components/Header";
 import ListOfCats from "../components/ListOfCategories";
 import ShelterNearestYou from "../components/ShelterNearestYou";
-import { GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+// import { GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import useFetch from '../functions/useFetch'
+import { GoogleMapProvider, InfoWindow, MapBox , Marker, GoogleMapContext} from '@googlemap-react/core'
+import latlngDist from 'latlng-distance'
+import { GoogleApiWrapper } from 'google-maps-react'
 
 const mapStyles = {
   map: {
@@ -15,14 +19,14 @@ const mapStyles = {
   }
 };
 
-class CategoriesView extends React.Component {
-  constructor(props) {
-    super(props);
-    //Initial State for info window, marker, and selected place
-    this.state = {
+const CategoriesView = (props) => {
+  const [state, setState] = useReducer(
+    
+    (state, newState) => ({...state, ...newState}),
+    {
       currentLocation: {
         lat: 40.785091,
-        lng: -73.968285
+        lon: -73.968285
       },
       showingInfoWindow: false,
       activeMarker: {},
@@ -30,176 +34,121 @@ class CategoriesView extends React.Component {
       centerAroundCurrentLocation: false,
       visible: true,
       zoom: 14
-    };
-  }
-
-  //On a click, the marker appears, place is selected, info window is visible
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: this.state.selectedPlace,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-  // Info window closes and marker is no longer visible
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
     }
-  };
-  //loadMap() function is only called after the component has been rendered
-  //and grabs a reference to the DOM component to where we want our map to be placed.
+  )
 
-  loadMap() {
-    if (this.props.google) {
-      // checks if google is available
-      const { google } = this.props;
-      const maps = google.maps;
 
-      const mapRef = this.refs.map;
-
-      // reference to the actual DOM element
-      const node = ReactDOM.findDOMNode(mapRef);
-
-      let { zoom } = this.state;
-      const { lat, lng } = this.state.currentLocation;
-      const center = new maps.LatLng(lat, lng);
-      this.setState({
-        centerAroundCurrentLocation: true
-      });
-      const mapConfig = Object.assign(
-        {},
-        {
-          center: center,
-          zoom: zoom
-        }
-      );
-
-      // maps.Map() is constructor that instantiates the map
-      this.map = new maps.Map(node, mapConfig);
-    }
-  }
-
-  //when the map has already loaded, componentDidMount() will set a call-
-  //back to fetch the current location.
-
-  componentDidMount() {
+ 
+  
+  let listOfShelters = []
+  useEffect(() => {
     if (navigator && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(pos => {
+        
         const coords = pos.coords;
-        this.setState({
+        setState({
           currentLocation: {
             lat: coords.latitude,
-            lng: coords.longitude
+            lon: coords.longitude,
           }
         });
       });
+      
     }
-    this.loadMap();
+    return listOfShelters
+  }, [])
+
+  listOfShelters = useFetch(`https://empact-e511a.firebaseio.com/shelters/all.json`)
+  console.log(listOfShelters)
+
+  const style = Object.assign({}, mapStyles.map);
+
+  let newShelters = []
+  let id = 0
+
+  for (let i=0; i < listOfShelters.length; i++) {
+    const lat = Number(listOfShelters[i].latitude)
+    const lon = Number(listOfShelters[i].longitude)
+    const point = {lat, lon}
+    console.log('shelter', point)
+    console.log('current', state.currentLocation)
+    const dist = latlngDist.distanceDiffInKm(point, state.currentLocation )
+    console.log('distance', dist)
+    const shelter = Object.assign(listOfShelters[i], {distance: dist, id: id, latitude: lat, longitude: lon})
+    newShelters.push(shelter)
+    id++
   }
 
-  //Check if map API is loaded, check if the browser's current location
-  //is provided and recenter the map to it.
+  const sortArrayOfObjects = (arr, key) => {
+    return arr.sort((a, b) => {
+        return a[key] - b[key];
+    });
+  };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
-    }
-    if (prevState.currentLocation !== this.state.currentLocation) {
-      this.recenterMap();
-    }
-  }
+  sortArrayOfObjects(newShelters, "distance");
 
-  //recenterMap() function only gets called when the currentLocation in the
-  //component's state is updated and uses the .panTo() method on the google.maps.Map
-  //instance to change the center of the map.
+  const firstShelter = newShelters[0]
 
-  recenterMap() {
-    const map = this.map;
-    const current = this.state.currentLocation;
+  console.log(state.currentLocation)
+  const { google } = props;
 
-    const google = this.props.google;
-    const maps = google.maps;
 
-    if (map) {
-      let center = new maps.LatLng(current.lat, current.lng);
-      map.panTo(center);
-    }
-  }
-
-  render() {
-    const { google } = this.props;
-
-    var origin1 = new google.maps.LatLng(55.930385, -3.118425);
-    var origin2 = "Greenwich, England";
-    var destinationA = "Stockholm, Sweden";
-    var destinationB = new google.maps.LatLng(50.087692, 14.42115);
-
-    var service = new google.maps.DistanceMatrixService();
+  if (firstShelter !== undefined) {
+    let destination = new google.maps.LatLng(firstShelter.latitude, firstShelter.longitude);
+    let origin = new google.maps.LatLng(state.currentLocation.lat, state.currentLocation.lon);
+    console.log('coordinates', destination, origin)
+  
+    let service = new google.maps.DistanceMatrixService();
     service.getDistanceMatrix(
-      {
-        origins: [origin1, origin2],
-        destinations: [destinationA, destinationB],
-        travelMode: "DRIVING"
-      },
+    {
+      origins: [origin],
+      destinations: [destination],
+      travelMode: "DRIVING",
+      unitSystem: google.maps.UnitSystem.IMPERIAL
+    },
       callback
     );
-
+  
     function callback(response, status) {
-      // See Parsing the Results for
-      // the basics of a callback function.
-      console.log(response);
+        // See Parsing the Results for
+        // the basics of a callback function.
+        console.log(response)
+    console.log(response.rows[0].elements[0].distance.text);
     }
+  }
 
-    // let origin = new google.maps.LatLng(
-    //   this.state.currentLocation.lat,
-    //   this.state.currentLocation.lng
-    // );
-    // let destination = new google.maps.LatLng(40.745377, -73.981306);
-
-    // google.maps.DistanceMatrixService(
-    //   { origin: origin, destination: destination, travelMode: "TRANSIT" },
-    //   (res, status) => {
-    //     if (status === "OK") {
-    //       console.log("distances", res);
-    //     } else {
-    //       console.log("status", status);
-    //     }
-    //   }
-    // );
-    const style = Object.assign({}, mapStyles.map);
-    console.log(this.props);
-    console.log("state", this.state);
-
-    return (
-      <div>
+  
+  
+  return(
+    <GoogleMapProvider>
+    <div>
         <div>
-          <div style={style} ref="map">
+          <div style={style}>
             Loading map...
           </div>
         </div>
-        <div>
-          <Marker onClick={this.onMarkerClick} name={"current location"} />
-          <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-            onClose={this.onClose}
-          >
-            <div>
-              <h4>{this.state.selectedPlace}</h4>
-            </div>
-          </InfoWindow>
-        </div>
-        <Header {...this.props} />
+        <Header />
         <ListOfCats />
       </div>
-    );
+      <MapBox
+      style={style}
+      apiKey="AIzaSyD2VA4VZXz5Hj7mr7s4L8Oybt1rX2fp7f4"
+      opts={{
+        center: {lat: state.currentLocation.lat, lng: state.currentLocation.lon},
+        zoom: 14,
+      }}
+    />
+    {newShelters.length > 0 ? <div>
+      <p>Closest Shelter</p>
+      <p>{newShelters[0].name}</p>
+    </div> : <div>Loading Shelters</div> }
+    
+    </GoogleMapProvider>
+  )
   }
-}
+  
+  export default GoogleApiWrapper({
+    apiKey: "AIzaSyD2VA4VZXz5Hj7mr7s4L8Oybt1rX2fp7f4"
+  })(CategoriesView)
+  
 
-export default GoogleApiWrapper({
-  apiKey: "AIzaSyD2VA4VZXz5Hj7mr7s4L8Oybt1rX2fp7f4"
-})(CategoriesView);
