6cbe5af49169072a7577027a556a7fc977cb70df
Emily McClanahan <efmcclanahan@gmail.com>
2019-04-10 17:46:18 -0500

    adds functional search

--- frontend/client/src/components/searchBar.js
+++ frontend/client/src/components/searchBar.js
@@ -3,7 +3,7 @@ import algoliasearch from "algoliasearch/lite";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
 
 const searchClient = algoliasearch(
-  "empact",
+  "QD6TWFQZCN",
   "028bde3e8ce26fd3245e84b3807905b9"
 );
 
@@ -12,7 +12,7 @@ const hitCom = props => {
   return <div>{props.hit.name}</div>;
 };
 
-class App extends React.Component {
+class SearchBar extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -39,7 +39,7 @@ class App extends React.Component {
       <div>
         {this.state.searchEnabled === true ? (
           <div>
-            <InstantSearch indexName="bestbuy" searchClient={searchClient}>
+            <InstantSearch indexName="empact" searchClient={searchClient}>
               <div onClick={this.disableSearch}>CLOSE SEARCH</div>
               <SearchBox />
               <Hits hitComponent={hitCom} />
