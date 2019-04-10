3e926a890d78be1c59c32b9d846faf01b972d8fc
Emily McClanahan <efmcclanahan@gmail.com>
2019-04-10 16:12:56 -0500

    Revert "Merge pull request #57 from labs11-homeless-services/caitlin-giguere"
    This reverts commit 5fcda35155184eb5a0c39fd80d2f7d3a2a2dbc13, reversing
    changes made to 1eaf599970991f16c0cb4ee34fe362142770fd79.

--- frontend/client/src/components/TabNavigator.js
+++ frontend/client/src/components/TabNavigator.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import useFetch from "../../functions/useFetch";
+import useFetch from "../functions/useFetch";
 import ListOfResources from "./ListOfResources";
 
 const TabNav = props => {
