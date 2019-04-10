0690e4503b0e0782556bf02733e317e7b08a0c30
josharmantrout91 <josh.armantrout@gmail.com>
2019-04-10 18:11:53 -0400

    Refactored file structure to match Design Architecture - LandingView and CategoriesView successuflly refactored.

--- frontend/client/src/views/subCategoryView.js
+++ frontend/client/src/views/subCategoryView.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import Header from '../components/Header'
-import TabNav from '../components/TabNavigator'
+import React from "react";
+import Header from "../components/Header";
+import TabNav from "../components/TabNavigator";
 
-const subCategoryView = (props) => {
-    return(
-        <div>
-            <Header />
-            <TabNav match={props.match} />
-        </div>
-    )
-}
+const subCategoryView = props => {
+  return (
+    <div>
+      <Header />
+      <TabNav match={props.match} />
+    </div>
+  );
+};
 
-export default subCategoryView
+export default subCategoryView;
