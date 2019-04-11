782c5b4fa36d0cdf5206e08624b70fc2db6a8c3a
McNultyPT <mcnultypt@gmail.com>
2019-04-11 14:35:26 -0700

    styling SearchBar Component

--- frontend/client/src/components/searchBar.js
+++ frontend/client/src/components/searchBar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import styled from 'styled-components';
+
 import { Link } from 'react-router-dom'
 import algoliasearch from "algoliasearch/lite";
 import { InstantSearch, SearchBox, Hits } from "react-instantsearch-dom";
@@ -7,6 +9,37 @@ const searchClient = algoliasearch(
   "QD6TWFQZCN",
   "028bde3e8ce26fd3245e84b3807905b9"
 );
+
+const FakeSearchInput = styled.input`
+  width: 355px;
+  height: 43px;
+  padding-left: 10px;
+  font-size: 11px;
+  border-radius: 2px;
+`;
+
+const FakeSearchInputContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 10px;
+`;
+
+const FakeSearchButton = styled.div`
+  width: 90px;
+  height: 43px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 12px;
+  background-color: #656176;
+  border-radius: 2px;
+  color: white;
+  margin: 0 20px 0 5px;
+`;
+
+const InstantSearchContainer = styled.div`
+  margin-top: 100px;
+`;
 
 const hitCom = props => {
   console.log(props);
@@ -17,7 +50,6 @@ const hitCom = props => {
       </div>
     </Link>
     ) ;
-
 };
 
 class SearchBar extends React.Component {
@@ -46,15 +78,18 @@ class SearchBar extends React.Component {
     return (
       <div>
         {this.state.searchEnabled === true ? (
-          <div>
+          <InstantSearchContainer>
             <InstantSearch indexName="empact" searchClient={searchClient}>
               <div onClick={this.disableSearch}>CLOSE SEARCH</div>
               <SearchBox />
               <Hits hitComponent={hitCom} />
             </InstantSearch>
-          </div>
+          </InstantSearchContainer>
         ) : (
-          <div onClick={this.enableSearch}>Search Bar</div>
+          <FakeSearchInputContainer onClick={this.enableSearch}>
+            <FakeSearchInput placeholder= 'search for resources you need' />
+            <FakeSearchButton>SEARCH</FakeSearchButton>
+          </FakeSearchInputContainer>
         )}
       </div>
     );
