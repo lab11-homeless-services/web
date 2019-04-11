f63db653937b2871f039dabdf3282128425c438a
McNultyPT <mcnultypt@gmail.com>
2019-04-11 14:53:20 -0700

    adding box shadow to header elements

--- frontend/client/src/components/searchBar.js
+++ frontend/client/src/components/searchBar.js
@@ -16,6 +16,7 @@ const FakeSearchInput = styled.input`
   padding-left: 10px;
   font-size: 11px;
   border-radius: 2px;
+  box-shadow: 1px 3px #00000020;
 `;
 
 const FakeSearchInputContainer = styled.div`
@@ -35,6 +36,7 @@ const FakeSearchButton = styled.div`
   border-radius: 2px;
   color: white;
   margin: 0 20px 0 5px;
+  box-shadow: 1px 3px #00000020;
 `;
 
 const InstantSearchContainer = styled.div`
