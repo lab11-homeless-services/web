61413c0734bd98437bbb493c9a76001bb8aef427
McNultyPT <mcnultypt@gmail.com>
2019-04-12 14:53:13 -0700

    adjusting margin on ShelterNearestYou component

--- frontend/client/src/components/ShelterNearestYou.js
+++ frontend/client/src/components/ShelterNearestYou.js
@@ -20,7 +20,7 @@ const ShelterNearestCard = styled.div`
   display: flex;
   flex-direction: row-reverse;
   align-items: center;
-  margin: 11% 0 0 2%;
+  margin: 12% 0 0 2%;
   box-shadow: 0px 0px 4px 2px #00000050;
 `;
 
