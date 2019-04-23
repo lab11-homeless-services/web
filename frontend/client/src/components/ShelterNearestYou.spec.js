b2aa2f74d4fd094a11282892c345de666253cc58
matt0418 <mfeldma7@gmail.com>
2019-04-23 18:42:09 -0400

    Some more tests in a few components

--- frontend/client/src/components/ShelterNearestYou.spec.js
+++ frontend/client/src/components/ShelterNearestYou.spec.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import LandingSearchBar from './LandingSearchBar'
+import ShelterNearestYou from './ShelterNearestYou'
 import renderer from 'react-test-renderer'
 import { render, fireEvent } from 'react-testing-library'
 
 describe('<LandingSearchBar />', () => {
     it('Header renders without crashing', () => {
-        render(<LandingSearchBar />)
+       const {getByText} = render(<ShelterNearestYou />)
+       const map = getByText('Loading...')
+        expect(map).toBeTruthy()
     })
     it('matches snapshot' ,() => {
-        const tree = renderer.create(<LandingSearchBar></LandingSearchBar>)
+        const tree = renderer.create(<ShelterNearestYou></ShelterNearestYou>)
         expect(tree.toJSON()).toMatchSnapshot()
     })
 })
